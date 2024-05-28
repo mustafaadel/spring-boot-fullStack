3e917938c41b63e1f6f35adc5d70e4214153cb33
moustafa adel <moustafa_adel@ecards.com.eg>
2024-05-28 16:45:49 +0300

    .

--- backend/src/main/java/com/atom/fullstack/SpringBootExampleApplication.java
+++ backend/src/main/java/com/atom/fullstack/SpringBootExampleApplication.java
@@ -13,7 +13,6 @@ public class SpringBootExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootExampleApplication.class, args);
-		
 	}
 	@Bean
 	CommandLineRunner runner(CustomerRepository customerRepository){
